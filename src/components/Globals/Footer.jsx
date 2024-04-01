a7f8a3bbd399b55ad971ad84f0c7d0cb094230a3
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-04-02 00:55:07 +0530

    footer corrected

--- src/components/Globals/Footer.jsx
+++ src/components/Globals/Footer.jsx
@@ -12,63 +12,63 @@ const Footer = () => {
                         </div>
                         <div className="address">
                             <div className="locate mb-4 text-sm font-extralight">- LOCATE US</div>
-                            <div className="uni-name text-lg mt-5 mb-4">Lovely Professional University</div>
-                            <div className="uni-address text-sm font-light mb-2">Jalandhar-Delhi, G.T. Road, Phagwara, Punjab (INDIA) -144411.</div>
+                            {/* <div className="uni-name text-lg mt-5 mb-4">Swami Vivekanand Institute of Engineering and Technology</div> */}
+                            <div className="uni-address text-sm font-light mb-2">Village- Ramnagar Near Banur, Tehsil- Rajpura, District- Patiala</div>
                         </div>
                         <div className="uni-contact ext-sm font-light mb-2">
-                            <a href="tel:+911824517000">Tel: +91-1824-517000</a> <br />
-                            <a href="tel:+911824404404">Tel: +91-1824-404404</a>
+                            <p>Admission helpline Number :</p>
+                            <p>+91- 94652 33333</p>
+                            <p>Toll Free: 1800 120 1200</p>
+                            <p>Email:</p>
+                            <p>Admission@sviet.ac.in</p>
+                            <p>info@sviet.ac.in</p>
+                            <p>tpo@sviet.ac.in</p>
+                            <p>hr@sviet.ac.in</p>
                         </div>
-                        <div className="get-directions">
-                            <a href="https://www.lpu.in/admission/lpu-in-your-town.php#lpu-town" className="link-btn">
-                                <img src="https://www.lpu.in/lpu-assets/images/icons/chevron-right.svg" alt="Chevron Right" />
-                                Get Directions
-                            </a>
-                        </div>
+                       
                     </div>
                     {/* Admissions section */}
                     <div className="w-full md:w-1/2 lg:w-1/4 mb-8 md:mt-2 p-6 border-r border-gray-400">
-                        <div className="heading text-xl font-semibold mb-4">Admissions</div>
+                        <div className="heading text-xl font-semibold mb-4">Quick Links</div>
                         <ul className="leading-loose text-sm font-light">
-                            <li><a href="https://www.lpu.in/admission/admissions.php">Admissions 2024-25</a></li>
-                            <li><a target="_blank" href="https://www.lpu.in/international/international_students.php">International Admission 2024-25</a></li>
-                            <li><a target="_blank" href="https://www.lpude.in/admissions/overview.php">Distance Education Admissions</a></li>
-                            <li><a target="_blank" href="https://www.lpuonline.com/">Online Education Admissions</a></li>
-                            <li><a href="https://www.lpu.in/scholarship/scholarship.php">Scholarship &amp; Aid</a></li>
-                            <li><a href="https://www.lpu.in/frmloginaccounts.aspx">Fee Deposits</a></li>
-                            <li><a href="https://www.lpu.in/downloads/faq.pdf">FAQs</a></li>
+                            <li><a href="#">Student ERP Login</a></li>
+                            <li><a href="#">Faculty ERP Login</a></li>
+                            <li><a href="#">SVGOI in your town</a></li>
+                            <li><a href="#">E-Brochure</a></li>
+                            <li><a href="#">How to Apply</a></li>
+                            <li><a href="#">SVIET – ITI</a></li>
+                            <li><a href="#">Ideathon</a></li>
+                            <li><a href="#">Techathon</a></li>
                         </ul>
                     </div>
                     {/* Academics section */}
                     <div className="w-full md:w-1/2 lg:w-1/4 mb-8 p-6 border-r border-gray-400">
-                        <div className="heading text-xl font-semibold mb-4">Academics</div>
+                        <div className="heading text-xl font-semibold mb-4">Quick Links</div>
                         <ul className="leading-loose text-sm font-light">
-                            <li><a href="https://www.lpu.in/jpd">Joint Placement Drive</a></li>
-                            <li><a href="https://www.lpu.in/educationawards">Education Awards</a></li>
-                            <li><a href="https://www.lpu.in/lpusummerschool/index.php">LPU Summer School</a></li>
-                            <li><a href="http://alumni.lpu.in/">Alumni</a></li>
-                            <li><a href="https://www.lpu.in/campus_life/entrepreneurship.php">Entrepreneurship</a></li>
-                            <li><a target="_blank" href="https://lpu.in/downloads/application.pdf">Recognition Application (ODL)</a></li>
-                            <li><a target="_blank" href="https://lpu.in/downloads/application-2.pdf">Recognition Application (Online)</a></li>
-                            <li><a title="Top Engineering Colleges (B.Tech - M.Tech)" href="//www.lpu.in/engineering/" target="_blank">Top engineering college in india</a></li>
-                            <li><a title="Top MBA Colleges" href="//www.lpu.in/mba/" target="_blank">Top MBA Colleges</a></li>
+                            <li><a href="#">Placement Overview</a></li>
+                            <li><a href="#">Career at SVGOI</a></li>
+                            <li><a href="#">Recognitions Approvals</a></li>
+                            <li><a href="#">Video Gallery</a></li>
+                            <li><a href="#">Photo Gallery</a></li>
+                            <li><a href="#">Grievances/Enquiry</a></li>
+                            <li><a href="#">Final Year 2023 Group Pictures</a></li>
+                            <li><a href="#">IQAC</a></li>
+                            <li><a href="#">NAAC</a></li>
                         </ul>
                     </div>
                     {/* Resources section */}
                     <div className="w-full md:w-1/2 lg:w-1/4 mb-8 p-6">
-                        <div className="heading text-xl font-semibold mb-4">Resources</div>
+                        <div className="heading text-xl font-semibold mb-4">Quick Links</div>
                         <ul className="leading-loose text-sm font-light">
-                            <li>Explorica</li>
-                            <li>Knowledge Brainstorm</li>
-                            <li>11th Convocation Gallery</li>
-                            <li> Distance Education</li>
-                            <li>Online Education</li>
-                            <li>Online Fee Payment</li>
-                            <li>UMS Login</li>
-                            <li>Apply Certificate</li>
+                            <li><a href="#">NCC Registration</a></li>
+                            <li><a href="#">SVGOI Advantages</a></li>
+                            <li><a href="#">Contact–us</a></li>
+                            <li><a href="#">Scholarship</a></li>
+                            <li><a href="#">Refund Policy</a></li>
+                            <li><a href="#">ERP APP</a></li>
+                            <li><a href="#">NSS Registration</a></li>
                         </ul>
                     </div>
-
                 </div>
                 {/* Footer bottom section */}
                 <div className="footer-bottom mt-8 border-t border-gray-700 pt-6">
