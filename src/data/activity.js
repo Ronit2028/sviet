2fae9c8069a51b18d5612e1901c7f5a321abc29e
Your Name <you@example.com>
2024-04-18 00:34:13 +0530

    changes

--- src/data/activity.js
+++ src/data/activity.js
@@ -1,43 +1,101 @@
 const activities = [
-        {
-          "id": 1,
-          "image": "https://scontent.fixc4-1.fna.fbcdn.net/v/t39.30808-6/336922432_2747792045363548_2609600354789932941_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=N83VOFsmVbgAb5BLoAa&_nc_oc=AdidIG4gV3p13F-0ur4tuKJ3Le0UYVdZF88tgFh-PG1WMLNY9xSEjzUe8ajUbCSP8udjeVMkUSj5BPNb411xr3eb&_nc_ht=scontent.fixc4-1.fna&oh=00_AfAOEiALgFOshp8e1tM9pJ_PdMIJvaeHc15Bpp713NL92g&oe=6625C52C",
-          "name": "Science Fiesta",
-          "desc": "The department of Applied Science organized a Science Fiesta Program for the students of 1st year.",
-          "program_type": "Engineering",
-          "link": "https://www.instagram.com/p/CqNSHDwyPP7/?utm_source=ig_web_copy_link"
-        },
-        {
-          "id": 2,
-          "image": "",
-          "name": "Guest Lecture",
-          "desc": "Department of CSE of SVIET has organized A guest lecturer and Q& A session on Programming.",
-          "program_type": "Engineering",
-          "link": "Read More"
-        },
-        {
-          "id": 3,
-          "image": "",
-          "name": "Talk Show",
-          "desc": "The CEO/Founder of Chai Thela, Mr. Pankaj Judge, shared his life-changing journey during Talk Show: Success Mantra.",
-          "program_type": "Engineering",
-          "link": "Read More"
-        },
-        {
-          "id": 4,
-          "image": "",
-          "name": "Super 60 Selection",
-          "desc": "Recently SVIET, Chandigarh has organized an Entrance test for the selection of new batch of super 60 students SUPER602K22.",
-          "program_type": "Engineering",
-          "link": "View More"
-        },
-        {
-          "id": 5,
-          "image": "",
-          "name": "Interview for S60 Batch",
-          "desc": "The selection process for the students of Super 60 & The Uniques batch 2022 has come to an end.",
-          "program_type": "Engineering",
-          "link": "View More"
-        }
+  {
+    "id": 1,
+    "image": "https://scontent.fixc4-1.fna.fbcdn.net/v/t39.30808-6/336922432_2747792045363548_2609600354789932941_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=N83VOFsmVbgAb5BLoAa&_nc_oc=AdidIG4gV3p13F-0ur4tuKJ3Le0UYVdZF88tgFh-PG1WMLNY9xSEjzUe8ajUbCSP8udjeVMkUSj5BPNb411xr3eb&_nc_ht=scontent.fixc4-1.fna&oh=00_AfAOEiALgFOshp8e1tM9pJ_PdMIJvaeHc15Bpp713NL92g&oe=6625C52C",
+    "name": "Science Fiesta",
+    "desc": "The department of Applied Science organized a Science Fiesta Program for the students of 1st year.",
+    "program_type": "Engineering",
+    "link": "https://www.instagram.com/p/CqNSHDwyPP7/?utm_source=ig_web_copy_link"
+  },
+  {
+    "id": 2,
+    "image": "",
+    "name": "Guest Lecture",
+    "desc": "Department of CSE of SVIET has organized A guest lecturer and Q& A session on Programming.",
+    "program_type": "Engineering",
+    "link": "Read More"
+  },
+  {
+    "id": 3,
+    "image": "",
+    "name": "Talk Show",
+    "desc": "The CEO/Founder of Chai Thela, Mr. Pankaj Judge, shared his life-changing journey during Talk Show: Success Mantra.",
+    "program_type": "Engineering",
+    "link": "Read More"
+  },
+  {
+    "id": 4,
+    "image": "",
+    "name": "Super 60 Selection",
+    "desc": "Recently SVIET, Chandigarh has organized an Entrance test for the selection of new batch of super 60 students SUPER602K22.",
+    "program_type": "Engineering",
+    "link": "View More"
+  },
+  {
+    "id": 5,
+    "image": "",
+    "name": "Interview for S60 Batch",
+    "desc": "The selection process for the students of Super 60 & The Uniques batch 2022 has come to an end.",
+    "program_type": "Engineering",
+    "link": "View More"
+  },
+  {
+    "id": 13,
+    "name": "Mocktail Session",
+    "desc": "A mocktail session was organized by Department of Hotel Management on 22nd September, 2022 for its students.",
+    "program_type": "Hotel-Management",
+    "image": "https://m.facebook.com/photo.php?fbid=416703610569119&set=a.405861254986688&type=3",
+    "link": "View More"
+  },
+  {
+    "id": 14,
+    "name": "Indian Cuisine Practical",
+    "desc": "Department of Hotel Management organized a INDIAN CUISINE PRACTICAL for the students of BHMCT- 2nd Sem.",
+    "program_type": "Hotel-Management",
+    "image": "indian_cuisine_image_url",
+    "link": "View More"
+  },
+  {
+    "id": 15,
+    "name": "Rajasthani Cuisine Practical",
+    "desc": "Department of Hotel Management organized a practical on RAJASTHANI CUISINE for the students of BHMCT- 4TH Sem.",
+    "program_type": "Hotel-Management",
+    "image": "rajasthani_cuisine_image_url",
+    "link": "View More"
+  },
+
+  {
+    "id": 16,
+    "name": "Interactive Seminar",
+    "desc": "Swami Vivekanand College of Law organised an interactive seminar on Alternative Dispute Resolution (ADR) on 10th March, 2023.",
+    "program_type": "law",
+    "image": "interactive_seminar_image_url",
+    "link": "View More"
+  },
+  {
+    "id": 17,
+    "name": "Inauguration",
+    "desc": "Mr. Suvir Sidhu, Advocate, Chairman, Bar Council of Punjab & Haryana inaugurated the Moot Court.",
+    "program_type": "law",
+    "image": "inauguration_image_url",
+    "link": "View More"
+  },
+  {
+    "id": 18,
+    "name": "Indian Constitution Day",
+    "desc": "Swami Vivekanand College of Law organized Inter-department Poster making competition on the topic “India the mother of Democracy”.",
+    "program_type": "law",
+    "image": "indian_constitution_day_image_url",
+    "link": "View More"
+  },
+  {
+    "id": 19,
+    "name": "Interactive Seminar",
+    "desc": "Swami Vivekanand College of Law organised an interactive seminar on Alternative Dispute Resolution (ADR) on 10th March, 2023.",
+    "program_type": "law",
+    "image": "interactive_seminar_image_url",
+    "link": "View More"
+  },
+
 ]
 export default activities;
