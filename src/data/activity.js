589c33329d4db1a7e4c920cf289c8ce0c5a8e09f
nirajn45 <guptaniraj915@gmail.com>
2024-04-17 23:16:28 +0530

    changed

--- src/data/activity.js
+++ src/data/activity.js
@@ -1,10 +1,43 @@
 const activities = [
-    {
-        id:1,
-        name:"",
-        desc:"",
-        program_type:"",
-        link:""
-    },
+        {
+          "id": 1,
+          "image": "https://scontent.fixc4-1.fna.fbcdn.net/v/t39.30808-6/336922432_2747792045363548_2609600354789932941_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=N83VOFsmVbgAb5BLoAa&_nc_oc=AdidIG4gV3p13F-0ur4tuKJ3Le0UYVdZF88tgFh-PG1WMLNY9xSEjzUe8ajUbCSP8udjeVMkUSj5BPNb411xr3eb&_nc_ht=scontent.fixc4-1.fna&oh=00_AfAOEiALgFOshp8e1tM9pJ_PdMIJvaeHc15Bpp713NL92g&oe=6625C52C",
+          "name": "Science Fiesta",
+          "desc": "The department of Applied Science organized a Science Fiesta Program for the students of 1st year.",
+          "program_type": "Engineering",
+          "link": "https://www.instagram.com/p/CqNSHDwyPP7/?utm_source=ig_web_copy_link"
+        },
+        {
+          "id": 2,
+          "image": "",
+          "name": "Guest Lecture",
+          "desc": "Department of CSE of SVIET has organized A guest lecturer and Q& A session on Programming.",
+          "program_type": "Engineering",
+          "link": "Read More"
+        },
+        {
+          "id": 3,
+          "image": "",
+          "name": "Talk Show",
+          "desc": "The CEO/Founder of Chai Thela, Mr. Pankaj Judge, shared his life-changing journey during Talk Show: Success Mantra.",
+          "program_type": "Engineering",
+          "link": "Read More"
+        },
+        {
+          "id": 4,
+          "image": "",
+          "name": "Super 60 Selection",
+          "desc": "Recently SVIET, Chandigarh has organized an Entrance test for the selection of new batch of super 60 students SUPER602K22.",
+          "program_type": "Engineering",
+          "link": "View More"
+        },
+        {
+          "id": 5,
+          "image": "",
+          "name": "Interview for S60 Batch",
+          "desc": "The selection process for the students of Super 60 & The Uniques batch 2022 has come to an end.",
+          "program_type": "Engineering",
+          "link": "View More"
+        }
 ]
-export default activities
+export default activities;
