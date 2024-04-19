7870051b671a257124b759cf65a9ef5ec7863752
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-04-19 12:05:53 +0530

    autoplay

--- src/components/LandingPage/Unlock.jsx
+++ src/components/LandingPage/Unlock.jsx
@@ -59,7 +59,9 @@ const Unlock = () => {
             infinite: true,
             speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            speed: 500,
+            autoplay: true,
         };
 
     return (
