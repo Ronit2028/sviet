b753990bfda2f74ed0ac49c92503a7772e71deac
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-04-20 10:42:58 +0530

    media upadtes

--- src/components/LandingPage/SVIETinMedia.jsx
+++ src/components/LandingPage/SVIETinMedia.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+"use client"
+
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SVIETinMedia = () => {
+
+    const mediaUpdates = [
+        {
+            imageUrl: "https://utfs.io/f/f265baa5-de33-44b9-a5f3-73cbdff7e0cc-mbjn4f.png",
+            name: "Spontania 2K24",
+            type: "Annual Cultural Fest",
+            desc: "Trending Now: Spontania 2024 Grabs Media Spotlight!Keep your eyes peeled for ongoing media reports from the Spontania 2024! There's always something new on the horizon. Join SVIET and be part of the change!",
+            date: "April 13-14, 2024"
+        },
+        {
+            imageUrl: "https://utfs.io/f/ac77c3d6-12fc-4a14-9808-a8e99f92e291-mbjn2n.png",
+            name: "Global Future Summit 2024",
+            type: "Industrialist & HR Panel Discssion and Ideathon",
+            desc: "Global Futures Summit 2024 Grabs Media Spotlight!Keep your eyes peeled for ongoing media reports from the Global Futures Summit 2024! There's always something new on the horizon. Join SVIET and be part of the change!      </p>",
+            date: "March 28, 2024"
+        },
+    ]
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplay: true,
+    };
+
     return (
         <div className='py-12 my-10 w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 gap-5'>
             <div className="py-10 w-full flex flex-col justify-center">
@@ -17,30 +49,38 @@ const SVIETinMedia = () => {
                 <button className='text-lg text-white rounded-md bg-[#fea700] w-fit p-2 mt-5'>Know More</button>
             </div>
             <div className="w-full ">
-                <div className="event-card rounded-md shadow-md">
-                    <div className="image-section h-[40vh]" style={{ backgroundImage: `url(https://media.licdn.com/dms/image/D5622AQG9N5MX7IwGSw/feedshare-shrink_2048_1536/0/1711190034649?e=1714608000&v=beta&t=ZJeC8VG0beXNZ7HILu03bao2vOUXJWNMcGwycTSBR4g)`, backgroundSize: "cover", backgroundPosition: "center bottom", backgroundRepeat: "no-repeat" }}>
+                <Slider {...settings}>
+                    {
+                        mediaUpdates.map((update, index) => (
+                            <div className="event-card rounded-md shadow-md" key={index}>
+                                <div className="image-section h-[40vh]" style={{ backgroundImage: `url(${update.imageUrl})`, backgroundSize: "cover", backgroundPosition: "center bottom", backgroundRepeat: "no-repeat" }}>
 
-                    </div>
-                    <div className="text-section p-6">
-                        <div>
-                            <p className="text-xl font-semibold ">
-                                Global Future Summit 2024
-                            </p>
-                            <p className='text-md pb-6'>
-                                Industrialist & HR Panel Discssion and Ideathon
-                            </p>
-                            <p className='text-sm pb-6'>
-                                Global Futures Summit 2024 Grabs Media Spotlight!
-                                Keep your eyes peeled for ongoing media reports from the Global Futures Summit 2024! There's always something new on the horizon. Join SVIET and be part of the change!      </p>
-                        </div>
-                        <div className='flex justify-between'>
-                            <p className="text-sm font-semibold text-[#fea700]">
-                                March 28, 2024
-                            </p>
-                            {/* <button className='rounded-md bg-[#fea700] text-white p-2 h-7 w-7 flex justify-center items-center'><i className='fa fa-arrow-right'></i></button> */}
-                        </div>
-                    </div>
-                </div>
+                                </div>
+                                <div className="text-section p-6">
+                                    <div>
+                                        <p className="text-xl font-semibold ">
+                                            {update.name}
+                                        </p>
+                                        <p className='text-md pb-6'>
+                                            {update.type}n
+                                        </p>
+                                        <p className='text-sm pb-6'>
+                                            {
+                                                update.desc
+                                            }
+                                        </p>
+                                    </div>
+                                    <div className='flex justify-between'>
+                                        <p className="text-sm font-semibold text-[#fea700]">
+                                            {update.date}
+                                        </p>
+                                        {/* <button className='rounded-md bg-[#fea700] text-white p-2 h-7 w-7 flex justify-center items-center'><i className='fa fa-arrow-right'></i></button> */}
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </Slider>
             </div>
         </div>
     )
