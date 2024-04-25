546d1ce446f6cc949dd1884bf255e86f98a30ae1
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-04-25 15:43:42 +0530

    more changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -39,16 +39,14 @@ export default function Home() {
         Enquire Now
       </button>
       {showModal && (
-        <div className="fixed inset-0 bg-white/50 bg-opacity-50 flex justify-center items-center z-30">
-          <div className=" w-4/5 lg:w-1/3 mx-auto rounded-lg p-4 z-50">
+        <div className="fixed inset-0 bg-white bg-opacity-50 flex justify-center items-center z-50">
+          <div className="w-4/5 lg:w-1/3 mx-auto rounded-lg p-4 z-50">
             <button className="absolute top-0 right-0 m-2 text-black hover:text-gray-800" onClick={handleCloseModal}>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
-
             <BookReleaseWidget />
-
           </div>
         </div>
       )}
