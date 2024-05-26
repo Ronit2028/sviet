8b631387a560c5ba5aa7c5d65c629210b62fac78
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-05-26 23:18:09 +0530

    svgoi in town fixed

--- src/components/Globals/Footer.jsx
+++ src/components/Globals/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
                         <ul className="leading-loose text-sm font-light">
                             <li><a href="https://uj.servergi.com:8079/ISIMSVIET/login">Student ERP Login</a></li>
                             <li><a href="https://uj.servergi.com:8079/SIMWEBSVIET/Login">Faculty ERP Login</a></li>
-                            <li><a href="#">SVGOI in your town</a></li>
+                            <li><Link href="/svgoi-in-town">SVGOI in your town</Link></li>
                             <li><a href="#" download={'/public/ebrochure.pdf'}>E-Brochure</a></li>
                             <li><a href="https://admission.sviet.ac.in/">How to Apply</a></li>
                             <li><a href="http://svietiti.in/index.html">SVIET - ITI</a></li>
