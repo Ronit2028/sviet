3e2518e2e3047bcecb44aed2cfbd0060fa457522
Niraj Kumar <guptaniraj915@gmail.com>
2025-01-29 10:22:43 +0530

    done

--- src/app/institutions/[id]/page.jsx
+++ src/app/institutions/[id]/page.jsx
@@ -39,8 +39,9 @@ const Page = ({ params }) => {
                     ]
                 }
             },
-            committee:"/SVIET/SVIET Committees.pdf",
+            committee:"/SVIET/SVIET-Committees.pdf",
             md:"/SVIET/Mandatory-Disclosure-SVIET-1.pdf",
+            
 
         },
         {
