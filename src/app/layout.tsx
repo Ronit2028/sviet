b5e91424368f57e7f473a3bb714dc73c2a7ff1a6
Niraj Kumar <guptaniraj915@gmail.com>
2025-05-07 22:15:06 +0530

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import '@fortawesome/fontawesome-free/css/all.css'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-import { ComplexNavbar } from "@/components/Globals/Nav"
-import Footer from "@/components/Globals/Footer"
-import Chatbot from '@/components/Globals/Chatbot'
-
-
+import { ComplexNavbar } from "@/components/Globals/Nav";
+import Footer from "@/components/Globals/Footer";
+import Chatbot from "@/components/Globals/Chatbot";
 
 const inter = Raleway({ subsets: ["latin"] });
 
@@ -21,8 +19,7 @@ export const metadata: Metadata = {
   description: "Be Future Ready",
   icons: {
     icon: "./icon.png",
-  }
-
+  },
 };
 
 export default function RootLayout({
@@ -30,7 +27,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
       <head>
@@ -41,17 +37,33 @@ export default function RootLayout({
 
         {children}
         <div className="fixed flex flex-col gap-2 end-0 bottom-0 z-30">
-          <a href="https://wa.me/7347411011" className="shadow-sm p-2 mr-8 mt-[-175px] bg-green-700 text-white h-13 w-13 text-5xl flex items-center justify-center rounded-full mb-3">
+          <a
+            href="https://wa.me/7347411011"
+            className="shadow-sm p-2 mr-8 mt-[-175px] bg-green-700 text-white h-13 w-13 text-5xl flex items-center justify-center rounded-full mb-3"
+          >
             <i className="fab fa-whatsapp"></i>
           </a>
-        
-            <div>
-            {!["/admission-bihar", "/admission-bihar1", "/admission-bihar2", "/admission-bihar3", "/admission-bihar4", "/admission-bihar5", "/admission-bihar6", "/admission-bihar7", "/admission-bihar8"].includes(
-              typeof window !== "undefined" ? window.location.pathname : ""
-            ) && (
-              <Chatbot />
-            )}
-            </div>
+
+          <div>
+            {typeof window !== "undefined" &&
+              (() => {
+                const currentPath = window.location.pathname.replace(/\/$/, "");
+                console.log("Current Path:", currentPath);
+                return (
+                  ![
+                    "/admission-bihar",
+                    "/admission-bihar1",
+                    "/admission-bihar2",
+                    "/admission-bihar3",
+                    "/admission-bihar4",
+                    "/admission-bihar5",
+                    "/admission-bihar6",
+                    "/admission-bihar7",
+                    "/admission-bihar8",
+                  ].includes(currentPath) && <Chatbot />
+                );
+              })()}
+          </div>
         </div>
         <Footer />
       </body>
